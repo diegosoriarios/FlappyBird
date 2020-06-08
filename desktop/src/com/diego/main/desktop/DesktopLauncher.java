53077e3051c4879dc4d6ca06b29d081ec1cfb974
diegosoriarios <diegosoriarios@gmail.com>
2020-06-08 00:10:23 -0300

    first commit

---
+++ desktop/src/com/diego/main/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.diego.main.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.diego.main.Game;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = Game.WIDTH;
+		config.height = Game.HEIGHT;
+		config.title = Game.TITLE;
+		new LwjglApplication(new Game(), config);
+	}
+}
